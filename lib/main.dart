d5a12c81c6d0c7f473c30c09f7ecfac367fde22e
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2024-10-08 22:59:29 +0200

    working on axis

--- lib/main.dart
+++ lib/main.dart
@@ -17,17 +17,47 @@ class _MainPageState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          backgroundColor: Colors.white,
+          backgroundColor: Colors.red,
           appBar: AppBar(
             backgroundColor: Colors.blue,
             title: Text("App Name"),
           ),
-          body: Center(
-            child: Text(
-              'Number of donuts: ${nDonuts}',
-              style: TextStyle(fontSize: 40),
-            ),
+          body: SafeArea(
+            child: Column(
+              //mainAxisSize: MainAxisSize.min,  //la column si ferma al size dei widget, dont go to bottom of phone
+              //verticalDirection: VerticalDirection.up, //stack widgets from bottom, .down stack from the top
+              mainAxisAlignment: MainAxisAlignment.center, //start/center/end/spaceround/spaceEvely/..
+              crossAxisAlignment: CrossAxisAlignment.stretch,  //stretch/start/end/center/..
+              children: [
+                Container(
+                  color: Colors.white,
+                  height: 100.0,
+                  width: 100.0,
+//margin: EdgeInsets.symmetric(vertical: 50.0,horizontal: 10.0),
+//margin: EdgeInsets.fromLTRB(20, 50, 20, 50),
+                  margin: EdgeInsets.only(bottom: 20),
+                  padding: EdgeInsets.all(20),
+                  child: Text("Container 1"),
+                ),
+                SizedBox(height: 20,),  //work as margin
+                Container(
+                  width: 100, height: 100,
+                  color: Colors.blue,
+                  child: Text("Container 2")
+                ),
+                Container(
+                    width: 100, height: 100,
+                    color: Colors.black26,
+                    child: Text("Container 2")
+                ),
+                Container(
+                  width: double.infinity,  //strecth to all width
+                  height: 10,  //as debug to see it
+                )
+              ],
+            )
           ),
+
           floatingActionButton: FloatingActionButton(
             onPressed:() { setState(() {
               nDonuts++;
