942faa30fff89e519bab152060c30ae758581027
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2024-10-08 12:05:32 +0200

    added action button

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,41 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home:Scaffold(
-        backgroundColor: Colors.tealAccent,
-        body: Container(),
-      )
-    )
+      MyApp()
   );
 }
+
+class MyApp extends StatefulWidget {
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MyApp> {
+  int nDonuts = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            title: Text("App Name"),
+          ),
+          body: Center(
+            child: Text(
+              'Number of donuts: ${nDonuts}',
+              style: TextStyle(fontSize: 40),
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed:() { setState(() {
+              nDonuts++;
+            });},
+          child: Icon(Icons.add),
+          backgroundColor: Colors.blue,
+
+        )
+    ));
+  }
+}
+
