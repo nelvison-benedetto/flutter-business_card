1dcf84f4dd479554552482d101b45832e09b4771
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2024-10-10 14:09:30 +0200

    project done business card

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ void main() {
 
 class MyApp extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
+    //create and return the instance of the class status associated to _MainPageState
 }
 
 class _MainPageState extends State<MyApp> {
@@ -16,6 +17,136 @@ class _MainPageState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,  //DON'T SHOW BANNER "DEBUG" ON PHONE
+      home: Scaffold(
+        backgroundColor: Colors.green,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              //Imagine Selfie
+              CircleAvatar(
+                radius: 84,
+                backgroundImage: AssetImage('images/Selfie_BkLaSpezia.jpg'),
+              ),
+              SizedBox(height: 7,),
+
+              //Nelvison Benedetto
+              Text(
+                "Nelvison Benedetto",
+                style: TextStyle(
+                  fontFamily: "Pacifico",
+                  fontSize: 32,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 2,),
+
+              //Flutter Developer
+              Text(
+                "flutter developer".toUpperCase(),
+                style: TextStyle(
+                  fontFamily: "SourceCodePro",
+                  fontSize: 22,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 8,),
+
+              //Divider (<hr>)
+              Container(
+                width: MediaQuery.of(context).size.width*0.45,
+                child: Divider(
+                  color: Colors.white,
+                  thickness: 2,
+                ),
+              ),
+              SizedBox(height: 16,),
+
+              //Tel + Email
+              Center(
+                child: Container(
+                  decoration: BoxDecoration(
+                    //color: Colors.orangeAccent,
+                  ),
+                  //padding: EdgeInsets.symmetric(horizontal: 60),
+                  width: MediaQuery.of(context).size.width * 0.92,
+
+                  child: Column(
+                    //crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        //height: 50, //width: 50,
+                          //padding: EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+                          padding : EdgeInsets.fromLTRB(10, 10, 0, 10),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                          child : Row(
+                            mainAxisSize: MainAxisSize.max,
+                             children: [
+                               Icon(
+                                 Icons.phone,
+                                 size: 22,
+                                 color: Colors.green,
+                               ),
+                               SizedBox(width: 10,),
+                               Text(
+                                 "+39 555 666 777",
+                                 style: TextStyle(
+                                   color: Colors.green,
+                                   fontSize: 19,
+                                 ),
+                               )
+                             ],
+                          )
+                      ),
+                      SizedBox(height: 10,),
+                      Container(
+                        //padding: EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+                        padding : EdgeInsets.fromLTRB(10, 10, 0, 10),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Icon(
+                              Icons.email,
+                              size: 22,
+                              color: Colors.green,
+                            ),
+                            SizedBox(width: 10,),
+                            Text(
+                              "nelvison.benedetto@gmail.com",
+                              style: TextStyle(
+                                color: Colors.green,
+                                fontSize: 19,
+                              ),
+                            )
+                          ]
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              /*Container(
+                //height: 10,
+                width: double.infinity,
+              )*/
+            ],
+          )
+        )
+      ),
+    );
+
+    /*return MaterialApp(
         home: Scaffold(
           backgroundColor: Colors.red,
           appBar: AppBar(
@@ -50,9 +181,18 @@ class _MainPageState extends State<MyApp> {
                     color: Colors.black26,
                     child: Text("Container 2")
                 ),
-                Container(
-                  width: double.infinity,  //strecth to all width
-                  height: 10,  //as debug to see it
+                Align(  //don't inherit the stretch applied to all other containers
+                alignment: Alignment.center,
+                child: Container(
+                  color: Colors.yellow,
+                  height: 100,width: 100,
+                ),
+              ),
+                Container(  //invisible container
+                  width: double.infinity,
+                    // used to align other containers base on this max width
+                    //to stretch all widgets use CrossAxisAlignment.stretch
+                  //height: 10,  //as debug to see it
                 )
               ],
             )
@@ -66,7 +206,7 @@ class _MainPageState extends State<MyApp> {
           backgroundColor: Colors.blue,
 
         )
-    ));
+    ));*/
   }
 }
 
